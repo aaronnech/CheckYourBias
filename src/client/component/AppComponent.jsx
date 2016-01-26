91986fb51cc5c33ca4d2244dbac3e1037d23918c
Aaron Nech <aaronnech@gmail.com>
2016-01-26 08:47:42 -0800

    Added scaffolding for screens to client side application

--- src/client/component/AppComponent.jsx
+++ src/client/component/AppComponent.jsx
@@ -1,15 +1,26 @@
 var React = require('react');
+var Constant = require('../Constant');
 
 var AppComponent = React.createClass({
     getInitialState : function() {
-        return {};
+      return {
+          currentScreen : Constant.SCREEN.HOME
+      };
+    },
+    
+    onSelectPage : function(page) {
+       this.setState({
+           currentScreen : page  
+       });
     },
 
     render : function() {
         return (
-            <div id="app">
-                I Am ALIVE!!
-            </div>
+          <div id="app">
+              <div className={this.state.currentScreen == Constant.SCREEN.HOME ? 'show' : 'hide'}>
+                 <p>Home Screen!</p>
+              </div>
+          </div>
         );
 	}
 });
