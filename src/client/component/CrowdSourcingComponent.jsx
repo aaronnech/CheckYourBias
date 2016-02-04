4525183ea26979887f33b98c2cdbbf976512efcb
Sonja Khan <sonjak3@uw.edu>
2016-02-04 13:17:18 -0800

    skeleton for crowdsourcing component

---
+++ src/client/component/CrowdSourcingComponent.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var Card = require('material-ui/lib/card/card');
+var CardTitle = require('material-ui/lib/card/card-title');
+var CardText = require('material-ui/lib/card/card-text');
+
+var CrowdsourcingComponent = React.createClass({
+	onSelectPage : function(page) {
+		this.setState({
+			currentScreen: page
+		})
+	},
+
+	render : function() {
+		return (
+			<Card className="submit-content">
+				<CardTitle title="Submit Content" />
+				<CardText>
+					<div className="main-content">
+						<p>Form will go here</p>
+					</div>
+				</CardText>
+			</Card>
+		);
+	}
+});
+
+module.exports = CrowdsourcingComponent;
