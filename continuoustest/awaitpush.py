9581e378816618c3f2ca9813c62646b0e3be1239
Aaron Nech <aaronnech@gmail.com>
2016-02-05 15:15:21 -0800

    fixed contious testing output

--- continuoustest/awaitpush.py
+++ continuoustest/awaitpush.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def continousTest():
+	print "Pulling repo..."
 	# Pull the repo
 	output = subprocess.check_output(["git", "pull"])
 
-	# Build the project
-	build_output = subprocess.check_output(["npm", "run-script", "make"])
+	print "Building project and running tests..."
+	# Build the project and test
+	build_output = subprocess.check_output(["npm", "run-script", "test"])
 
-	# Run tests
-	test_output = subprocess.check_output(["npm", "run-script", "test"])
-
-	msg = MIMEText("Hello! I am CYB build/test bot. A push happened and I tried to build and test. Here are the results: \n\n %r" % (build_output + test_output))
+	print "Emailing results..."
+	msg = MIMEText("Hello! I am CYB build/test bot. A push happened and I tried to build and test. Here are the results: \n\n %r" % (build_output))
 	msg["From"] = "build.test@cyb.org"
 	msg["To"] = "aaronnech@gmail.com"
 	msg["Subject"] = "CYB - Build / Test Results"
-	p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+	p = subprocess.Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
 	p.communicate(msg.as_string())
 
 if __name__ == '__main__':
