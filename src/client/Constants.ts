0c407d00880d4a015ddc68dc74c8d47b97bcc6cf
Sonja Khan <sonjak3@uw.edu>
2016-02-04 23:03:39 -0800

    adding form for direct quotes

--- src/client/Constants.ts
+++ src/client/Constants.ts
@@ -12,6 +12,24 @@ class Constants {
 		FULL_NAME: "userFullName"
 	};
 
+	public static CONTENT_TYPES: any = [
+		'Direct Quote',
+		'Summary of Legislation',
+		'General',
+	];
+
+	public static CANDIDATES: any = [
+		'Hillary Clinton',
+		'Bernie Sanders',
+		'Ted Cruz',
+		'Donald Trump',
+		'Marco Rubio',
+	];
+
+	public static ERRORS: any = {
+		REQUIRED: 'This field is required',
+	}
+
 	public static FIREBASE_URL: string = "";
 	public static FIRE_USER: string = Constants.FIREBASE_URL + 'users/';
 	public static fireAccess: string = '';
---
+++ src/client/component/CrowdsourcingQuoteComponent.jsx
@@ -0,0 +1,93 @@
+var React = require('react');
+var Constants = require('../Constants');
+
+var AutoComplete = require('material-ui/lib/auto-complete');
+var TextField = require('material-ui/lib/text-field');
+
+const QUOTE_MAX = 400;
+
+var CrowdsourcingQuoteComponent = React.createClass({
+
+    getInitialState : function() {
+        return {
+            candidate: null,
+            candidateErrorText: Constants.ERRORS.REQUIRED,
+            quote: null,
+            quoteErrorText: Constants.ERRORS.REQUIRED,
+            source: null,
+            sourceErrorText: Constants.ERRORS.REQUIRED,
+        };
+    },
+
+    handleUpdateCandidate : function(value) {
+        var errorText = null;
+        if (value.length == 0) {
+            errorText = Constants.ERRORS.REQUIRED;
+        }
+        // TODO (sonjakhan): Add error check for invalid candidate
+        this.setState({
+            candidate: value,
+            candidateErrorText: errorText,
+        });
+    },
+
+    handleUpdateQuote : function(event) {
+        var quote = event.target.value;
+        var errorText = null;
+
+        if (quote.length == 0) {
+            errorText = Constants.ERRORS.REQUIRED;
+        } else if (quote.length > QUOTE_MAX) {
+            errorText = "Quote must be " + QUOTE_MAX + " characters or less";
+        }
+        this.setState({
+            quote: quote,
+            quoteErrorText: errorText,
+        });
+    },
+
+    handleUpdateSource : function(event) {
+        var source = event.target.value;
+        var errorText = null;
+
+        if (source.length == 0) {
+            errorText = Constants.ERRORS.REQUIRED;
+        } 
+
+        this.setState({
+            source: source,
+            sourceErrorText: errorText,
+        })
+    },
+
+    render : function() {
+        return (
+            <div>
+                <p>Which candidate said it?</p>
+                <AutoComplete
+                    hintText="Candidate"
+                    errorText={this.state.candidateErrorText}
+                    dataSource={Constants.CANDIDATES}
+                    filter={AutoComplete.caseInsensitiveFilter}
+                    onUpdateInput={this.handleUpdateCandidate}
+                />
+                <p>Quote:</p>
+                <TextField
+                    hintText="I will make America great again"
+                    errorText={this.state.quoteErrorText}
+                    multiLine={true}
+                    onChange={this.handleUpdateQuote}
+                />
+                <p>Source:</p>
+                <TextField
+                    hintText="Link to reliable source"
+                    errorText={this.state.sourceErrorText}
+                    multiLine={true}
+                    onChange={this.handleUpdateSource}
+                />
+            </div>
+        );
+    }
+});
+
+module.exports = CrowdsourcingQuoteComponent;
