7c357b933b24a13d9394c7a7c57e8beceab43969
Geoffrey Liu <geoffliu@uw.edu>
2016-02-06 14:32:48 -0800

    Fix styles on RateViewpointsComponent. Fixes #4

--- src/client/component/RateViewpointsComponent.jsx
+++ src/client/component/RateViewpointsComponent.jsx
@@ -4,7 +4,7 @@ var CardTitle = require('material-ui/lib/card/card-title');
 var CardText = require('material-ui/lib/card/card-text');
 var CardActions = require('material-ui/lib/card/card-actions');
 var Slider = require('material-ui/lib/slider');
-var FlatButton = require('material-ui/lib/flat-button');
+var RaisedButton = require('material-ui/lib/raised-button');
 
 var RateViewpointsComponent = React.createClass({
 	onSelectPage : function(page) {
@@ -14,34 +14,33 @@ var RateViewpointsComponent = React.createClass({
 	},
 
 	changeAgreement : function(event) {
-		console.log("User changed agreement level");
+		// TODO: store the new agreement level in the component state
+		// console.log("User changed agreement level");
 	},
 
 	confirmReaction : function() {
-		// show user who said the issue.
-		console.log("User confirmed reaction");
+		// TODO: show user the candidate who said the issue.
+		// console.log("User confirmed reaction");
 	},
 
 	render : function() {
 		return (
 			<Card className="rate-viewpoints">
 				<CardTitle title="Major issue!" />
-				<CardText>
-					<div className="issue-wrapper main-content">
-						<p>The text of the issue will appear here.</p>
-
-						<p>Right now this is just a placeholder.</p>
-					</div>
+				<CardText className="issue-wrapper">
+					<p>The text of the issue will appear here. Duis lectus ligula, fermentum sit amet sapien ut, aliquet varius ante. Sed sit amet gravida orci, eu mollis eros. Fusce vestibulum dolor quis massa tempor consectetur. Sed id placerat neque. Maecenas in justo eget sem faucibus maximus. Morbi sodales varius sem, quis ultricies velit tincidunt sed.
+					</p>
 				</CardText>
 				<CardActions>
 					<div className="rate-scale">
-						<Slider min={0} max={5} defaultValue={2.5} onChange={this.changeAgreement} />
-						<div className="agreement-labels">
-							<span id="disagree-label">Strongly disagree</span>
-							<span id="agree-label">Strongly agree</span>
+						<div className="slider-wrapper">
+							<input type="range" min="0" max="5" step="0.01"
+								onChange={this.changeAgreement} />
 						</div>
 					</div>
-					<FlatButton label="Confirm" primary={true} onClick={this.confirmReaction} />
+					<div className="confirm-choice-wrapper">
+						<RaisedButton label="Next" secondary={true} onClick={this.confirmReaction} style={{marginTop: '1em'}} />
+					</div>
 				</CardActions>
 			</Card>
 		);
