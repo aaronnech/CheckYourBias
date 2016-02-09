da400c550af1f1d4dd96052ebaed0ef886f01d3a
Sonja Khan <sonjak3@uw.edu>
2016-02-08 21:00:24 -0800

    adding some comments, will add more after I eat dinner

--- src/client/component/CrowdSourcingComponent.jsx
+++ src/client/component/CrowdSourcingComponent.jsx
@@ -11,8 +11,22 @@ var MenuItem = require('material-ui/lib/menus/menu-item');
 var TextField = require('material-ui/lib/text-field');
 var RaisedButton = require('material-ui/lib/raised-button');
 
+/**
+ * Form for a user to submit content to the application. The form is dynamic and 
+ * changes form fields based on the type of content the user is submitting.
+ *
+ * @author sonjakhan
+ */
 var CrowdsourcingComponent = React.createClass({
 
+
+    /**
+     * contentType: The Constants.CONTENT_TYPE the user is submitting 
+     * formComponent: The component to show based on the selected contentType
+     * category: The category from Constants.CATEGORIES the content belongs to
+     * source: A string representing a url to the source of the content
+     * sourceErrorText: The error text to show when there is an invalid source
+     */
     getInitialState : function() {
         return {
             contentType: 1,
@@ -23,6 +37,9 @@ var CrowdsourcingComponent = React.createClass({
         };
     },
 
+    /**
+     * Sets the form to match the selected content type
+     */
     handleContentType : function(event, index, value) {
         var activeForm = null;
         switch(Constants.CONTENT_TYPES[index]) {
@@ -40,12 +57,18 @@ var CrowdsourcingComponent = React.createClass({
         });
     },
 
+    /**
+     * Sets the category of the content to what the user selected
+     */
     handleCategory : function(event, index, value) {
         this.setState({
             category: value,
         });
     },
 
+    /**
+     * Sets the source of the content to what the user typed
+     */
     handleUpdateSource : function(event) {
         var source = event.target.value;
         var errorText = null;
