500d5a4c348753f3a70c0debef33789ff97682e5
Todd Maegerle <toddis@cs.washington.edu>
2016-02-15 15:32:38 -0800

    Implemented Candidate. Fixed Issue Firebase reference type/bug.

--- src/common/Candidate.ts
+++ src/common/Candidate.ts
@@ -20,11 +20,29 @@ class Candidate {
 	website: string;
 	firebaseRef: Firebase;
 
+	constructor(id: string, name: string, affiliatedParty: string,
+		issueRatings: { [key: string]: string },
+		website: string) {
+		this.name = name;
+		this.affiliatedParty = affiliatedParty;
+		this.issueRatings = issueRatings;
+		this.website = website;
+		this.firebaseRef = new Firebase(Constants.FIRE_CANDIDATE + "/" + id);
+	}
+
 	/*
 		Fetches the Candidate with the given candidateId.
 	*/
 	public static getCandidate(candidateId: string, callback: (candidate: Candidate) => any): void {
-		// TODO: Implement
+		var rootRef: Firebase = new Firebase(Constants.FIRE_CANDIDATE);
+		rootRef.child(candidateId).once("value", function(snapshot) {
+			var candidate = snapshot.val();
+			candidate.id = candidateId;
+			callback(candidate);
+		}, function(errorObject) {
+			// The id given was not valid or something went wrong.
+			console.log("The read failed" + errorObject.code);
+		});
 	}
 }
 
