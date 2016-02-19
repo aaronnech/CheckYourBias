53e5bf402e47a025b0da4de42336a170474e4995
Geoffrey Liu <main@g-liu.com>
2016-02-18 23:23:20 -0800

    Webdriver skeleton for menu functionality

---
+++ webdriver/ClickTheMenu.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+
+/**
+ * This test loads the application, and clicks the menu items
+ */
+describe('Menu', function() {
+	xit('shows up', function() {
+
+	});
+
+	xit('able to click each menu item', function() {
+
+	});
+});
